a776c203428bcce09d3e59af9c471786e5a2bb4c
rudravashishtha <rudraturtle7@gmail.com>
2024-05-23 08:31:27 -0700

    Draw Rectangles

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import math
 
 pygame.init()
 
-
+# Constants
 FPS = 60
 
 WIDTH, HEIGHT = 600, 600
@@ -27,6 +27,25 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
 
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS) 
+
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    draw_grid(window)
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
@@ -38,6 +57,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window)
 
     pygame.quit()
 
